2e6bdc6a1b87176b6045239a8544cb9f03aad64c
Flavia964 <flavia.lima643@gmail.com>
2021-03-26 19:45:56 -0300

    Exercício pronto.

--- resolucao-exercicio-01.js
+++ resolucao-exercicio-01.js
@@ -1,2 +1,9 @@
 // 1. Crie uma função que declara uma variável sem atribuir valor a ela
 // e retorna essa variável;
+
+let v;
+ function variavelSemValor() {
+    return v;
+}
+console.log(v);
+    variavelSemValor();
