4602034934f31ebcd4425542c9ded0c18b783c77
Flavia964 <flavia.lima643@gmail.com>
2021-03-26 20:32:09 -0300

    Exercício pronto.

---
+++ resolucao-exercicio-02.js
@@ -0,0 +1,9 @@
+// 2. Crie uma função que declara uma variável, atribuindo valor a ela
+// e retorna essa variável;
+
+function zoologico (){
+    let animal = "Girafa";
+    return animal
+}
+console.log(zoologico());
+
