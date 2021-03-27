5edadfaf6604140a87598bc7d58442b19398e174
Flavia964 <flavia.lima643@gmail.com>
2021-03-27 15:14:35 -0300

    Exercício pronto.

---
+++ resolucao-exercicio-08.js
@@ -0,0 +1,16 @@
+// 8. Crie uma função que classifica uma idade em jovem, meia-idade e idoso
+// jovens são aqueles que possuem até 39 anos;
+// meia-idade são os que estão entre 40 e 60 anos;
+// idosos são os que posssuem mais de 60 anos;
+// ex: classificaIdade(60) => "meia-idade"
+
+function categorizaAcordoComIdade (idade) {
+    if(idade <=39){
+        return "Jovem!";
+    } else if (idade > 60) {
+        return "Idoso!";
+    } else if (idade >=40) {
+        return "Meia- idade!";
+}
+}
+console.log(categorizaAcordoComIdade(60));
