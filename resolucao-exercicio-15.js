b3c1fbc82f3290402b8ad32c4aa9b54da4873864
Flavia964 <flavia.lima643@gmail.com>
2021-03-27 23:36:12 -0300

    Exercício pronto.

---
+++ resolucao-exercicio-15.js
@@ -0,0 +1,11 @@
+// 15. Crie uma função que verifica se um número é par sem usar if else
+// ex: isEven(12) => true;
+
+function retornaPar(n) {
+    if(n % 2 ===0) {
+    return true;
+    } else {
+    return false;
+}
+}
+console.log(retornaPar(12));
---
+++ resolucao-exercicio-16.js
@@ -0,0 +1,9 @@
+// 16. Crie uma função que verifica se um número é ímpar
+// ex: isOdd(-13) => true;
+
+function verificaSeImpar(a) {
+    if(a % 2 !== 0){
+    return true;
+    }
+}
+console.log(verificaSeImpar(3));
