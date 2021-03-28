a05f3bd921ceca873845e39866d1fe79392b3de5
Flavia964 <flavia.lima643@gmail.com>
2021-03-28 19:44:01 -0300

    Exercício pronto.

---
+++ resolucao-exercicio-18.js
@@ -0,0 +1,9 @@
+// 18. Crie uma função que verifica se um número é divisível por 3
+// ex: isDivisibleBy3(9) => true
+
+function numeroDivisivelPor3(numero) {
+    if(numero % 3 === 0) {
+        return  true;
+    }
+}
+console.log(numeroDivisivelPor3(15));
