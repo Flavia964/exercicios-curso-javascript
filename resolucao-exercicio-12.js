0463ca39a10ed59065d8662bfc80a0e4b3e7859f
Flavia964 <flavia.lima643@gmail.com>
2021-03-29 22:10:13 -0300

    Exercício pronto.

---
+++ resolucao-exercicio-12.js
@@ -0,0 +1,8 @@
+// 12. Crie uma função que retorna o valor máximo entre três números quaisquer
+// ex: max(12,4,8) => 12; max(12,4,18) => 18
+
+function imprimaMaiorValor(c, d, e) {
+
+    return Math.max(c,d,e);
+}
+console.log(imprimaMaiorValor(1, 51, 50));
