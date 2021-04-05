09bf0fde7e5b28df4b4ee09a0a56c6057ccdb766
Flavia964 <flavia.lima643@gmail.com>
2021-04-05 20:39:00 -0300

    Exercício pronto.

--- resolucao-exercicio-05.js
+++ resolucao-exercicio-05.js
@@ -1,2 +1,9 @@
 // 5. Crie uma função que verifica se um numero é maior que outro, SEM usar o
 // if e o else. ex: ehMaior(2,1) => true; ehMaior(1,2) => false
+
+
+function numeroEhMaior(n1, n2) {
+   let number  = n1 > n2;
+   return number;
+}
+console.log(numeroEhMaior(0, 8));
