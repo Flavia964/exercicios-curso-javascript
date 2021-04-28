d59c3af3cd620ce09d7e26cd0cc1f2f854a2ec6f
Flavia964 <flavia.lima643@gmail.com>
2021-04-27 22:33:36 -0300

    Exercício pronto.

---
+++ modulo-02/resolucao-exercicio-18.js
@@ -0,0 +1,13 @@
+// 18. Criar uma função que imprime todos os elementos de um array
+// que estão posicionados em índices múltiplos de 5.
+// imprime5(["hey",2,3,6,7,8,9,0,"olá"]) => imprime hey e 8
+
+function imprimeListaIndicesMultiplos5(lista){
+    for(let i= 0; i< lista.length; i++){
+        let el = lista [i];
+       if(i % 5 === 0){
+            console.log(el);
+        }
+    }
+}
+imprimeListaIndicesMultiplos5(["Pedro", 1, 2, 3, 4, "Maria"]);
